d5f05ed2931b1bb5b66b4f4d29e06fb185607931
Geno Enchev <gnenchev99@gmail.com>
2021-08-15 19:25:45 +0300

    Created the application
    The majority of the functionalities have been added

---
+++ GenoSolution/GenoSolution/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GenoSolution.Models;
+
+namespace GenoSolution.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<GenoSolution.Models.Product> Product { get; set; }
+    }
+}
---
+++ GenoSolution/GenoSolution/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenoSolution.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+        public DateTime DeliveryOn { get; set; }
+    }
+}
