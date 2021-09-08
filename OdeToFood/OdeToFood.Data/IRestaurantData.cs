496a5ddd6c8094454b0c4fbd892b052ef41e4ce8
Geno Enchev <gnenchev99@gmail.com>
2021-09-08 21:51:39 +0300

    Drilling into data

---
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Scott's Pizzaa", Location="Marryland", Cuisine=CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Geno's Food", Location="New York", Cuisine=CuisineType.Mexican},
+                new Restaurant {Id = 3, Name = "I am Ivan", Location="California", Cuisine=CuisineType.Indian}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
